59a842ea35fc3f368beb45cebb7470b6e8c56a6d
ermakovvova <ermakovvova@mail.r>
2017-03-19 15:02:12 -0700

    SecretSanta.pm changed

--- homeworks/santa/lib/SecretSanta.pm
+++ homeworks/santa/lib/SecretSanta.pm
@@ -12,50 +12,42 @@ use DDP;
 
 
 sub calculate {
-	my @members = @_;
-	#my $ref1 = ['Рита', 'Игорь'];
-	#my $ref2 = ['Вадим', 'Катя'];
-	#my @members = ('Али', 'Ира', $ref1, $ref2); 
+	my @members = @_; 
 	my @res;
 	my %presented;
-	#my $n = scalar @members + scalar grep { ref($_) } @members;
-	my %hash = map {@$_} grep{ ref($_) } @members;
-        %hash =  (%hash, reverse map {@$_} grep{ ref($_) } @members );
 	my @all = map {if (ref($_)) {@$_} else {$_}} @members;
 	my %uniq;
 	my @all_members = grep { !$uniq{$_}++ } @all;
-
-	
 	my $n = scalar @all_members;
-
-#	print "@members\n";
-#	p @members;
-#	print '@all_members:'."\n";	
-#	p @all_members;
-	#print '%hash:'."\n";
-	#p %hash;	
+	my %hash;
+		if ( (scalar grep {ref($_)} @members) != 0 ) {
+		%hash = map {@$_} grep{ ref($_) } @members;
+		%hash =  (%hash, reverse map {@$_} grep{ ref($_) } @members );
+	}
+	else {
+		my $non_friend1 = int(rand($n));
+		my $non_friend2;
+		do {
+			$non_friend2 = int(rand($n))
+		} while ($non_friend1 == $non_friend2);
+		%hash = (
+			$all_members[$non_friend1] => $all_members[$non_friend2],
+			$all_members[$non_friend2] => $all_members[$non_friend1],
+		)
+	}	
 	for (my $i = 0; $i < $n; $i++) {
-		print "$i\n";
+		#print "$i\n";
 		my $rand = int(rand($n));
-		print "$rand\n";
+		#print "$rand\n";
 		$presented { $all_members[$i] } = $all_members [ $rand ];
 		if ( $presented{$all_members[$i]} eq  $all_members[$i] ){
 			redo;
 		} 
 		if ( exists $hash{$all_members[$i]} and ($presented{$all_members[$i]} eq  $hash{$all_members[$i]})) {
 			redo;
-		}
-#		if ( exists $presented { $all_members[$rand] } and   $presented { $all_members[$rand] }  eq  $all_members[$i] ) {
-#                        redo;
-#		}
-#		p %presented;  
-	
-	
+		}	
 		push @res,[ "$all_members[$i]", "$presented{$all_members[$i]}" ];
 	}
-	#print '%presented:'."\n";
-	#p %presented;
-#	p @res;
 	return @res;
 
 }
