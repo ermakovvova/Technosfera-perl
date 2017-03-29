11a219b43ace5eb594bd9a52e10df11a54084f43
ermakovvova <ermakovvova@mail.r>
2017-03-29 13:23:37 -0700

    DeepClone.pm changed

--- homeworks/deep-clone/lib/DeepClone.pm
+++ homeworks/deep-clone/lib/DeepClone.pm
@@ -4,6 +4,8 @@ use 5.010;
 use strict;
 use warnings;
 
+use DDP;
+
 =encoding UTF8
 
 =head1 SYNOPSIS
@@ -36,11 +38,45 @@ use warnings;
 
 sub clone {
 	my $orig = shift;
+	#my $num = 10;
+	#my $orig = \$num;
+=for comment	
+	my $orig = {
+		key1 => "string",
+		key2 => [2, 4, 'a'],
+		key3 => {
+			nested => "value",
+			"key\0" => [1, 2]
+		}
+	};
+=cut
+	my @source;
+	my @result;
+	my @ref;
+	my $i = 0;
+	$source[$i] = $orig;
 	my $cloned;
-	# ...
-	# deep clone algorith here
-	# ...
+	
+	if ( !ref($source[$i])) {
+		$cloned = $source[$i];
+	}	
+	else {
+		given (ref($source[$i])) {
+
+			when ("HASH") {
+				$ref [$i] = {%{$source [$i]}}; 
+				foreach (keys %{$source [$i]}) {
+					
+				}
+			}
+
+			when("ARRAY") {$ref [$i]  = [@{$source [$i]}] }
+		}
+		$cloned = $ref [0];
+	}
+	#p %$cloned;
 	return $cloned;
 }
+#clone;
 
 1;
