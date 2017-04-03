556af1f291b35e6d7215ea052d344c6299e2f75b
ermakovvova <ermakovvova@mail.r>
2017-04-03 13:36:59 -0700

    SecretSanta.pm changed

--- homeworks/santa/lib/SecretSanta.pm
+++ homeworks/santa/lib/SecretSanta.pm
@@ -12,7 +12,7 @@ use DDP;
 
 
 sub calculate {
-	#my @members = ( 'Рита', 'Игорь', ['Вадим', 'Катя'], 'Али', 'Ира');
+	#my @members = ( 'Рита', 'Игорь', 'Вадим', ['Катя', 'Али'], 'Ира');
 	my @members = @_; 
 	my @res;
 	my %presented;
@@ -20,58 +20,47 @@ sub calculate {
 	my %uniq;
 	my @all_members = grep { !$uniq{$_}++ } @all;
 	my $n = scalar @all_members;
-	if ( $n > 3 ) {	
-		my %hash;
-		if ( (scalar grep {ref($_)} @members) != 0 ) {
+	if ( $n > 2 ) {	
+		if ( $n == 3 and ((scalar grep {ref($_)} @members) != 0) ) {
+			@res = ();
+		}
+		else {
+			my %hash;
 			%hash = map {@$_} grep{ ref($_) } @members;
 			my @all_members_temp = %hash;
 			%hash =  (%hash, reverse map {@$_} grep{ ref($_) } @members );
 			@all_members_temp[scalar @all_members_temp..$n-1] = grep{! exists $hash{$_}} @all_members;
 			@all_members = ();
 			@all_members = @all_members_temp;
-		}
-		else {
-			my $non_friend1 = int(rand($n));
-			my $non_friend2;
-			do {
-				$non_friend2 = int(rand($n))
-			} while ($non_friend1 == $non_friend2);
-			%hash = (
-				$all_members[$non_friend1] => $all_members[$non_friend2]
-			);
-			$hash { $all_members [$non_friend2] } = $all_members[$non_friend1];
-			my @all_members_temp;
-			$all_members_temp[0] = $all_members [$non_friend1];
-			$all_members_temp[1] = $all_members [$non_friend2];
-			@all_members_temp [2..$n-1] = grep {$_ ne ($all_members_temp[0]) and  $_ ne ($all_members_temp[1])} @all_members;
-			@all_members = ();
-			@all_members = @all_members_temp;
-		}
 		
-		for (my $i = 0; $i < $n; $i++) {
-			my $rand = int(rand($n));
-			if ( grep {$_ eq  $all_members[$rand]  } values %presented ) {
-	                        if ($i == $n-1) {
-					$i = 0;
-					%presented = ();
-					@res = ();
+			for (my $i = 0; $i < $n; $i++) {
+				my $rand = int(rand($n));
+				if ( grep {$_ eq  $all_members[$rand]  } values %presented ) {
+		       	                 if ($i == $n-1) {
+						$i = 0;
+						%presented = ();
+						@res = ();
+					}
+					redo;
+		      	  	}
+				$presented { $all_members[$i] } = $all_members [ $rand ];
+				if ( $presented{$all_members[$i]} eq  $all_members[$i] ){
+					redo;
+				} 
+				if ( exists $hash{$all_members[$i]} and ($presented{$all_members[$i]} eq  $hash{$all_members[$i]})) {
+					redo;
 				}
-				redo
-		        }
-			$presented { $all_members[$i] } = $all_members [ $rand ];
-			if ( $presented{$all_members[$i]} eq  $all_members[$i] ){
-				redo;
-			} 
-			if ( exists $hash{$all_members[$i]} and ($presented{$all_members[$i]} eq  $hash{$all_members[$i]})) {
-				redo;
+				if ( exists $presented{$all_members[$rand]} and $presented{$all_members[$rand]} eq $all_members[$i] ) { 
+					redo;
+				}
+				push @res,[ $all_members[$i], $presented{$all_members[$i]} ];
 			}
-			push @res,[ $all_members[$i], $presented{$all_members[$i]} ];
 		}
-		
 	}
 	else {
-		@res = ()
+		@res = ();
 	}
+	#p @res;
 	return @res;
 
 }
